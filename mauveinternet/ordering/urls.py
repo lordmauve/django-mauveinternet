c427ff96c858ead15ad5e893c51e75d07162c71b
daniel <daniel@localhost>
2008-05-12 11:41:18 +0000

    Added base template for orderitem views Added named url for viewing order item

--- mauveinternet/ordering/urls.py
+++ mauveinternet/ordering/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 	(r'^$', 'mauveinternet.ordering.views.orders_new'),
 	(r'^all$', 'mauveinternet.ordering.views.orders_all'),
-	(r'^(?P<code>[0-9]+)/$', 'mauveinternet.ordering.views.view_order'),
+	url(r'^(?P<code>[0-9]+)/$', 'mauveinternet.ordering.views.view_order', name='view-order'),
 	url(r'^(?P<code>[0-9]+)/', 'mauveinternet.ordering.views.view_order_item', name='view-order-item'),
 )
 
